b906260da198fb0841003ed29e6377142e0e0ac5
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-10-02 22:11:30 +0100

    I added vocals

--- blackjack.js
+++ blackjack.js
@@ -1,10 +1,14 @@
-
-// let nameInputEl = document.querySelector("#nameInput-el")
-// let playerName = nameInputEl.
-let player = {
-    name: "Ronald",
-    chips: 50
+function enterGame(){
+    const nameInputEl = document.getElementById('nameInput-el')
+    const playerName = nameInputEl.value
+    alert('Welcome back ' + playerName)
 }
+
+let player = {
+    name: 'Token',
+    chips: 100
+}
+
 let sum = 0
 let startedGame = false
 let hasBlackJack = false
@@ -16,7 +20,7 @@ let messageEl = document.getElementById('message-el')
 let playerEl = document.getElementById('player-el')
 playerEl.textContent += player.name + ':$' + player.chips
 let defImgEl = document.getElementById('defImg-el')
-// let Cards = []
+let Cards = []
 let CardsNumber = []
 let newCards = [
     '`<img src="CARDS/ACE.png">`',
@@ -41,7 +45,7 @@ function startGame() {
         let firstCard = getRandomCard()
         let secondCard = getRandomCard()
         sum = CardsNumber[0] + CardsNumber[1] + 2
-        // Cards =[firstCard,secondCard]
+         Cards =[firstCard,secondCard]
         renderGame()
     }
     startedGame = true
@@ -51,12 +55,11 @@ function getRandomCard() {
     CardsNumber.push(ActualRC)
     NrandomC = newCards[ActualRC]
     cardsContainerEl.innerHTML += NrandomC
-
 }
-
 function renderGame() {
     if (sum < 21) {
         message = "Do you want to draw another card?"
+
     } else if (sum === 21) {
         message = "Wohoo! you've got a black jack!"
         hasBlackJack = true
@@ -68,9 +71,19 @@ function renderGame() {
     }
     messageEl.textContent = message
     // for (let i = 0; i < Cards.length; i++){
-    //     cardsContainerEl.innerHTML += Cards[i]
-    // }
+        //     cardsContainerEl.innerHTML += Cards[i]
+        // }
+    
     sumEl.textContent = "Sum: " + sum
+    if (isAlive === false){
+        let reduction = player.chips / 2
+        let playerMoney = player.name + ':$' + reduction
+        playerEl.textContent = playerMoney
+    }else if (hasBlackJack === true){
+        let reduction = player.chips * 10
+        let playerMoney = player.name + ':$' + reduction
+        playerEl.textContent = playerMoney
+    }
 }
 
 let thirdCard = ''
